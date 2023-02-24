056462f11a2daaf50b8d4492f172f55c27239df7
cindyangXQ <yangc2105@gmail.com>
2023-02-25 02:18:54 +0800

    Create p1.js

---
+++ src/problem1/p1.js
@@ -0,0 +1,24 @@
+var sum_to_n_a = function(n) {
+    // your code here
+  let result = 0;
+  for (let i = 1; i <= n; i++) {
+    result += i;
+  }
+  return result;
+};
+
+var sum_to_n_b = function(n) {
+    // your code here
+  if (n <= 1) return n;
+  else return sum_to_n_b(n-1) + n;
+};
+
+var sum_to_n_c = function(n) {
+    // your code here
+  if (n<=1) return n;
+  else return (1+n) * n / 2;
+};
+
+console.log(sum_to_n_a(5));
+console.log(sum_to_n_b(5));
+console.log(sum_to_n_c(5));
