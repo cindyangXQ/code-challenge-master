5adbdbf11e566f4cf4e1005a367596fae39f0d27
cindyangXQ <yangc2105@gmail.com>
2023-02-27 00:04:35 +0800

    p3

--- src/problem3/datasource.js
+++ src/problem3/datasource.js
@@ -1,1 +1,36 @@
 // your code here:
+import fetch from 'node-fetch';
+
+class DataSource {
+    getPrices() {
+        return fetch("https://interview.switcheo.com/test.json")
+            .then((response) => response.json())
+            .then((js) => Object.values(js.data.prices))
+            .then((prices) => prices.map(price => 
+                    new Price(price.buy, price.sell, price.id, price.pair, price.timestamp)));
+    }
+}
+
+class Price {
+    constructor(buy, sell, id, pair, timestamp) {
+        this.buy = buy;
+        this.sell = sell;
+        this.pair = pair;
+    }
+
+    mid() {
+        return (this.buy + this.sell)/200;
+    }
+
+    quote() {
+        return this.pair.slice(3);
+    }
+}
+
+
+new DataSource().getPrices()
+    .then(prices => {
+        prices.forEach(price => {
+            console.log(`Mid price for ${ price.pair } is ${ price.mid() } ${ price.quote() }.`);
+        });
+    });
